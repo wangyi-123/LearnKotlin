40c295c0f60275cdb90ea92b227c5d2e6242bd64
wangyi <2798566332@qq.com>
2023-12-31 08:22:55 +0800

    Initial commit

---
+++ src/main/kotlin/Demo01.kt
@@ -0,0 +1,6 @@
+class Rect(var height:Int, var width:Int);
+
+fun main(args: Array<String>) {
+    var rect01 = Rect(20,10);
+    println("矩形的高度+${rect01.height}");
+}
---
+++ src/main/kotlin/WashMachine.kt
@@ -0,0 +1,14 @@
+class WashMachine(var module:String, var size:Int) {
+    fun openDoor(){
+        println("开门");
+    }
+
+    fun closeDoor(){
+        println("关门");
+    }
+    fun start(){
+        println("开始");
+
+    }
+
+}
